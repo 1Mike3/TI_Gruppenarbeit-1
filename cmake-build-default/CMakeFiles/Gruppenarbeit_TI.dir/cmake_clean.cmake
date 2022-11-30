fce8485d19dbc502d90b5a1a09362f341f71a56a
RefRexi <ic22b050@technikum-wien.at>
2022-11-30 18:32:41 +0100

    test emad

---
+++ cmake-build-default/CMakeFiles/Gruppenarbeit_TI.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Gruppenarbeit_TI.dir/cmake-build-debug/insertionsort.c.obj"
+  "CMakeFiles/Gruppenarbeit_TI.dir/cmake-build-debug/insertionsort.c.obj.d"
+  "CMakeFiles/Gruppenarbeit_TI.dir/cmake-build-debug/mergesort.c.obj"
+  "CMakeFiles/Gruppenarbeit_TI.dir/cmake-build-debug/mergesort.c.obj.d"
+  "CMakeFiles/Gruppenarbeit_TI.dir/cmake-build-debug/quicksort.c.obj"
+  "CMakeFiles/Gruppenarbeit_TI.dir/cmake-build-debug/quicksort.c.obj.d"
+  "CMakeFiles/Gruppenarbeit_TI.dir/main.c.obj"
+  "CMakeFiles/Gruppenarbeit_TI.dir/main.c.obj.d"
+  "Gruppenarbeit_TI.exe"
+  "Gruppenarbeit_TI.exe.manifest"
+  "Gruppenarbeit_TI.pdb"
+  "libGruppenarbeit_TI.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Gruppenarbeit_TI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-default/CMakeFiles/Gruppenarbeit_TI.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Gruppenarbeit_TI.
+# This may be replaced when dependencies are built.
