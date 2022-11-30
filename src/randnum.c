8f213cc99f2032f952cb8953d5af23bd48551643
RefRexi <ic22b050@technikum-wien.at>
2022-11-30 20:44:52 +0100

    emad add randnum.c

---
+++ src/randnum.c
@@ -0,0 +1,13 @@
+//
+// Created by Emad on 30/11/2022.
+//
+//not finished: function which returns a random number on a selected range
+#include <stdio.h>
+#include "stdlib.h"
+#include "time.h"
+
+unsigned int genRandNum(){
+    time_t t;
+    srand((unsigned) time(&t));
+    int rand();
+}
