b1268dac34eaea250b165d06ca2ae76aa9f90914
RefRexi <ic22b050@technikum-wien.at>
2022-11-30 20:44:45 +0100

    emad add randnum.c

--- src/_genFunc.c
+++ src/_genFunc.c
@@ -4,10 +4,3 @@
 #include <stdio.h>
 #include "stdlib.h"
 #include "time.h"
-//not finished: function which returns a random number on a selected range
-
-unsigned int genRandNum(){
-    time_t t;
-    srand((unsigned) time(&t));
-    int rand();
-}
