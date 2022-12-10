588b6665922f93610123f122c40d7f3fbf1b0763
Michi <michalkuc55@gmail.com>
2022-12-10 11:23:07 +0100

    Michi, Ordnerstuktur für implementation 1.1 geändert, an IMplementation 1.1 weitergearbeitet.

--- implementations/1.1implementation.c
+++ implementations/1.1implementation.c
@@ -2,7 +2,7 @@
 // Created by Michi on 10.12.2022.
 //
 #include "stdio.h"
-
+#include "..\include\allfunclib.h"
 //read in three arrays of sizes 8,16 and 64 and initialize them with random numbers
 //call sorting function 1 = mergesort, 2 = quicksort, 3 = Bubblesort, 4 = Insertion-sort
 //although obvious will pass size so easier adapting if requirements change
@@ -12,7 +12,19 @@ void basicCheck1_1(int call, int *arr8, int *arr16, int*arr64, int *sizeArr){
     switch (call) {
         //mergesort
         case 1:
-            *arr8 = 1;
+            printf("\n###### MERGESORT-IMPLEMENTATION #####\n\n");
+            printf("printout randomly initialized Array:\n");
+            genRandNum(*(sizeArr+0),arr8) ;
+            printRandArr(arr8, *(sizeArr+0));
+            printf("printout mergesort sorted Array:\n");
+            mergeS(arr8, *(sizeArr+0));
+            printRandArr(arr8, *(sizeArr+0));
+          //  printf("before emad randnum\n");
+          //  printArr(arr8, 8);
+
+          //  printf("after emad randnum \n");
+           // printArr(arr8, 8);
+            mergeS(arr8, *(sizeArr+0));
         break;
         //quicksort
         case 2:
