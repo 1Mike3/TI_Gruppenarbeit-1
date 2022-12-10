b6c5e5a989ac42d534900eaf9cd7456fa55563da
momoKitsune <tea.takemi@mailbox.org>
2022-12-10 12:43:05 +0100

    bubblesort.c und insertionsort.c von local nach GitHub

--- src/bubblesort.c
+++ src/bubblesort.c
@@ -1,7 +1,19 @@
 //
 // Created by Emad on 30/11/2022.
+// Edited by Mark on 10.12.2022.
 //
-#include "stdio.h"
+
+#include <stdio.h>
+
 void bubbleS (int *arr, unsigned int size){
-    printf("test");
+    int i, j, temp;
+    for (i = 0; i < size-1; i++) {
+        for (j = 0; j < size-i-1; j++) {
+            if (*(arr+j) > *(arr+j+1)) {
+                int temp = *(arr+j);
+                *(arr+j) = *(arr+j+1);
+                *(arr+j+1) = temp;
+            }
+        }
+    }
 }
--- src/insertionsort.c
+++ src/insertionsort.c
@@ -1,8 +1,18 @@
 //
 // Created by Michi on 30.11.2022.
+// Edited by Mark on 10.12.2022.
 //
 #include "stdio.h"
 
 void inserS (int *arr, unsigned int size){
-    printf("test");
+    int i, j, temp;
+    for (i = 1; i < size; i++) {
+        temp = *(arr+i);
+        j = i-1;
+        while (j >= 0 && *(arr+j) > temp) {
+            *(arr+j+1) = *(arr+j);
+            j = j-1;
+        }
+        *(arr+j+1) = temp;
+    }
 }
