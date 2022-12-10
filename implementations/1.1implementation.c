5baaa1da3d57cada9d23d8020e55dfe7bb9bcdce
Michi <michalkuc55@gmail.com>
2022-12-10 16:02:11 +0100

    Michi, Erste Tests Sortierfunktionen in 1.1implementation.c ... wenigstens funktionieren 50% der Sortieralgorythmen

--- implementations/1.1implementation.c
+++ implementations/1.1implementation.c
@@ -12,28 +12,67 @@ void basicCheck1_1(int call, int *arr8, int *arr16, int*arr64, int *sizeArr){
     switch (call) {
         //mergesort
         case 1:
+            /*
             printf("\n###### MERGESORT-IMPLEMENTATION #####\n\n");
             printf("printout randomly initialized Array:\n");
             genRandNum(*(sizeArr+0),arr8) ;
-            printRandArr(arr8, *(sizeArr+0));
+            printArr(arr8, *(sizeArr+0));
             printf("printout mergesort sorted Array:\n");
             mergeS(arr8, *(sizeArr+0));
-            printRandArr(arr8, *(sizeArr+0));
+            printArr(arr8, *(sizeArr+0));
+            printf("Check if the array is correctly sorted:\n");
+            checkSort(arr8,*(sizeArr+0),1);
           //  printf("before emad randnum\n");
           //  printArr(arr8, 8);
 
           //  printf("after emad randnum \n");
            // printArr(arr8, 8);
-            mergeS(arr8, *(sizeArr+0));
+
         break;
+             */
+
         //quicksort
         case 2:
+            /*
+            printf("\n###### Quicksort-IMPLEMENTATION #####\n\n");
+            printf("printout randomly initialized Array:\n");
+            int arr8test[8] = {2,4,7,2,8,5,3,3,};
+           // genRandNum(*(sizeArr+0),arr8) ;
+            printArr(arr8test, 8);
+            printf("printout quicksort sorted Array:\n");
+           // quickS(arr8test, 5);
+            printArr(arr8test, 8);
+            printf("Check if the array is correctly sorted:\n");
+           // checkSort(arr8test,8,1);
+             */
         break;
         //Bubblesort
         case 3:
+/*
+            printf("\n###### BUBBLESORT-IMPLEMENTATION #####\n\n");
+            printf("printout randomly initialized Array:\n");
+            genRandNum(*(sizeArr+0),arr8) ;
+            printArr(arr8, *(sizeArr+0));
+            printf("printout Bubblesort sorted Array:\n");
+            bubbleS(arr8, 8);
+            printArr(arr8, *(sizeArr+0));
+            printf("Check if the array is correctly sorted:\n");
+            checkSort(arr8,*(sizeArr+0),1);
+*/
         //Insertion-sort
         break;
         case 4:
+/*
+            printf("\n###### INSERTIONSORT-IMPLEMENTATION #####\n\n");
+            printf("printout randomly initialized Array:\n");
+            genRandNum(*(sizeArr+0),arr8) ;
+            printArr(arr8, *(sizeArr+0));
+            printf("printout INSERTIONSORT sorted Array:\n");
+            inserS(arr8, 8);
+            printArr(arr8, *(sizeArr+0));
+            printf("Check if the array is correctly sorted:\n");
+            checkSort(arr8,*(sizeArr+0),1);
+*/
         break;
 
         default:
