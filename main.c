b68ac4b5305389ba81f4ed5e36bf2006fc529a15
Michi <michalkuc55@gmail.com>
2022-12-11 14:13:25 +0100

    Michi, user abfrage in main hinzugefügt

--- main.c
+++ main.c
@@ -5,13 +5,15 @@
 
 int main() {
 
+    //TODO this is only a quick test make the function immune to wring input
     //============MICHI USER SELECTION =================
+    char inputString[] ="000";
    //let the User select which implementation will be tested (1.1 12 ...)
     printf("\n#######  SORTING & SEARCHING ALGORITHMS ####### \n\n");
     printf("\" G \"\n\n");
     printf("select the implementation you want to run: \n");
     printf("(possible input: 1.1, 1.2, 1.3, 1.4, all) \n");
-
+    scanf("%s", inputString);
 
     //END=========MICHI USER SELECTION ==============
 
@@ -37,18 +39,21 @@ int main() {
 */
 
     //Michi attempt at implementation 1.1
+
     //created the th arrays but have not initialized them, this will happen in the "basicCheck1_1 function with Emads rand func"
     printf("\nSTART implementation 1.1\n");
+    if (inputString == "1.1") {
+        int call = 1; //Variable that stands for the different sorting algorithms see 1.1implementation.c for further detail
+        const int arrSizePar[3] = {8, 16,
+                                   64}; //size parameter, determines the tested array sizes in the basic check function
 
-//
-    int call = 1; //Variable that stands for the different sorting algorithms see 1.1implementation.c for further detail
-   const int arrSizePar[3] = {8,16,64}; //size parameter, determines the tested array sizes in the basic check function
-
-    while (call < 5){ //while loop which increments the call variable so that a different sort. alg. is used every cycle.
-        basicCheck1_1(call, arrSizePar, 3);
-        call++;
+        while (call <
+               5) { //while loop which increments the call variable so that a different sort. alg. is used every cycle.
+            basicCheck1_1(call, arrSizePar, 3);
+            call++;
+        }
+        printf("\nEND implementation 1.1\n");
     }
-    printf("\nEND implementation 1.1\n");
     //Michi END attempt at implementation 1.1
 
     // bubbleS(*arr, size);
