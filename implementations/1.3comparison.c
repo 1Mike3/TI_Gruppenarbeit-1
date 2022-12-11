b7b1d2cce9eacbb6544055a9bce3c8d716951bab
Michi <michalkuc55@gmail.com>
2022-12-11 14:36:49 +0100

    Michi, kleines update

--- implementations/1.3comparison.c
+++ implementations/1.3comparison.c
@@ -1,6 +1,15 @@
 //
 // Created by Michi on 11.12.2022.
 //
-void comparison1_3(){
+#include "stdio.h"
+#include "stdlib.h"
+#include "..\include\allfunclib.h"
 
+//function which implements the 1.2 assignment
+void comparison1_3(){
+    const int arraySize = 2000;
+    const int cycleCount = 20;
+    int *arr[arraySize] = {};
+    genRandNum(arraySize, arr);
+    bubbleS(arr, arraySize);
 }
