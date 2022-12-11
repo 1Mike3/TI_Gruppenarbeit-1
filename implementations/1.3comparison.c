6e8a681ffc22cc80e747c0c9847b45b736f24fa9
momoKitsune <tea.takemi@mailbox.org>
2022-12-11 17:41:43 +0100

    1.3 first changes (what is max?) - working

--- implementations/1.3comparison.c
+++ implementations/1.3comparison.c
@@ -2,10 +2,10 @@
 // Created by Michi on 11.12.2022.
 //
 #include <time.h>
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include "..\include\allfunclib.h"
-
+/*
 //function which implements the 1.2 assignment
 void comparison1_3(){
     const int arraySize = 2000;
@@ -13,19 +13,30 @@ void comparison1_3(){
     int *arr[arraySize] = {};
     genRandNum(arraySize, arr);
     bubbleS(arr, arraySize);
-    //x20 times
-    for(int i = 0; i < cycleCount; i++){
-        bubbleS(arr, arraySize);
-    }
-    //track time x20 bubbleS
+    //do bubbleS 20 times
     clock_t start = clock();
-    for(int i = 0; i < cycleCount; i++){
+    for (int i = 0; i < cycleCount; i++) {
         bubbleS(arr, arraySize);
     }
     clock_t end = clock();
     double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
-    //avarage = time_spent / cycleCount
-    printf("The average time for 20 cycles of bubbleS is: %f seconds \n", time_spent / cycleCount);
-    //short = time_spent
-    printf("The shortest time for 20 cycles of bubbleS is: %f seconds \n", time_spent);
-    ??? LONG_LONG_MAX ???
+    printf("Time spent for bubble sort: %f seconds for %d cycles with %d elements in the array \n", time_spent, cycleCount, arraySize);
+    genRandNum(arraySize, arr);
+    bubbleS(arr, arraySize);
+    double minTime = time_spent;
+    double maxTime = time_spent;
+    for (int i = 0; i < cycleCount; i++) {
+        start = clock();
+        bubbleS(arr, arraySize);
+        end = clock();
+        time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+        if (time_spent < minTime){
+            minTime = time_spent;
+        }
+        if (time_spent > maxTime){
+            maxTime = time_spent;
+        }
+    }
+    printf("Min time for bubble sort: %f seconds for %d cycles with %d elements in the array \n", minTime, cycleCount, arraySize);
+    printf("Max time for bubble sort: %f seconds for %d cycles with %d elements in the array \n", maxTime, cycleCount, arraySize);
+*//
