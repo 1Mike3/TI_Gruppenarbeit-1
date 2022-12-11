c3965e101d16290fbf215fe862059a9c4b07dc17
momoKitsune <tea.takemi@mailbox.org>
2022-12-11 17:16:38 +0100

    1.3 first changes (what is max?)

--- implementations/1.3comparison.c
+++ implementations/1.3comparison.c
@@ -1,6 +1,7 @@
 //
 // Created by Michi on 11.12.2022.
 //
+#include <time.h>
 #include "stdio.h"
 #include "stdlib.h"
 #include "..\include\allfunclib.h"
@@ -12,4 +13,18 @@ void comparison1_3(){
     int *arr[arraySize] = {};
     genRandNum(arraySize, arr);
     bubbleS(arr, arraySize);
-}
+    //x20 times
+    for(int i = 0; i < cycleCount; i++){
+        bubbleS(arr, arraySize);
+    }
+    //track time x20 bubbleS
+    clock_t start = clock();
+    for(int i = 0; i < cycleCount; i++){
+        bubbleS(arr, arraySize);
+    }
+    clock_t end = clock();
+    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+    //avarage = time_spent / cycleCount
+    printf("The average time for 20 cycles of bubbleS is: %f seconds \n", time_spent / cycleCount);
+    //short = time_spent
+    printf("The shortest time for 20 cycles of bubbleS is: %f seconds \n", time_spent);
