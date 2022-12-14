bee18c4e22fb63ec9763b5961a659a9c6a5a6657
momoKitsune <tea.takemi@mailbox.org>
2022-12-14 11:12:00 +0100

    bubblesort mit kommentare

--- src/bubblesort.c
+++ src/bubblesort.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
+//takes two values in the array, compares them and swaps them if the first value is larger than the second value
 void bubbleS (int *arr, int size){
     int i, j, temp;
     for (i = 0; i < size-1; i++) {
         for (j = 0; j < size-i-1; j++) {
-            if (*(arr+j) > *(arr+j+1)) {
-                temp = *(arr+j);
+            if (*(arr+j) > *(arr+j+1)) {    //if the first value is larger than the second value
+                temp = *(arr+j);            //swap the values
                 *(arr+j) = *(arr+j+1);
                 *(arr+j+1) = temp;
             }
