a1e968c87c9b645659fa48fe70c18ea0fb2c2e8b
1Mike3 <michalkuc55@gmail.com>
2023-01-05 19:46:29 +0100

    Michi arvite an quicksortstring

--- src_2.x_Specific/quicksortString.c
+++ src_2.x_Specific/quicksortString.c
@@ -4,14 +4,19 @@
 
 #include "quicksortString.h"
 #include "allfunclib.h"
+#include "generateRandomStruct.h"
+#include "string.h"
+#include <stdlib.h>
+#include "stdio.h"
+#include "binarySearchAlgorithm.h"
 
 //will be changed to string
 
 //Used sub-functions, (further infos over implementation):
 //unsigned in for indexes chosen so don't waste space on negative array indexes
 //lower technically not needed but added to be able to sort arrays partially
-int chosePartition(int *arr, int lower, int upper);
-void sorting(int *arr,int lower, int upper);
+int chosePartition(data *dataArray, int lower, int upper);
+void sorting(data *dataArray ,int lower, int upper);
 
 
 
@@ -19,22 +24,26 @@ void sorting(int *arr,int lower, int upper);
 //"arr" is the chosen array of any size (edgecase size 0 behavior?)
 // "size" is the total number of elements the chosen array
 //!!watch out size is total number of items AND NOT last array index...
-void quickSortString(int *arr, int size){
+void quickSortString(data *dataArray, int size){
     int upperValue = (size-1);
-    sorting(arr, 0, upperValue);
+    sorting( dataArray, 0, upperValue);
 }
 
 //######################SUBFUNCTIONS######################################
 
 //function for choosing the Partition, will be recursively called in the "sorting function"
 //the lower and upper
-int chosePartition(int *arr,int lower,int upper){
-    int pivotValue = *(arr+upper); //initialising Pivot, always chosen as upper element
+int chosePartition(data *dataArray,int lower,int upper){
+
+    //initialising Pivot, always chosen as upper element
+    char *pivotValue = calloc(strlen((dataArray+upper)->string), sizeof(char ));
+    strcpy(pivotValue, (dataArray+upper)->string);
+
     int lowCounter = lower; // var which will be incremented in for loop
 
     for ( int i = lower; i <= upper; ++i) { //
-        if (*(arr+i) < pivotValue){
-            swapi((arr+lowCounter), (arr+i));
+        if (checkOrderString(pivotValue, (dataArray+i)->string)){ // OO (*(arr+i) < pivotValue){
+            swapi((arr+lowCounter), (arr+i));                     // OO swapi((arr+lowCounter), (arr+i));
             lowCounter++;
         }
     }
@@ -43,12 +52,18 @@ int chosePartition(int *arr,int lower,int upper){
 }
 
 //function which does the actual sorting
-void sorting(int *arr,int lower, int upper){
+void sorting(data *dataArray,int lower, int upper){
     if(lower < upper){
         int temp = chosePartition(arr , lower, upper);
         sorting(arr, lower, temp-1);
         sorting(arr, temp+1, upper);
     }
-
 }
 
+//function to swap two strings in an array
+//!!Untested
+void swapStr(char *val1,char *val2){
+    char *temp  = val1;
+    *val1 = *val2;
+    *val2 = temp;
+}
