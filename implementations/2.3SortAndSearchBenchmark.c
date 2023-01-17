29860d407281449bb9893bfb4dd08198b0c25712
Emad <ic22b050@technikum-wien.at>
2023-01-17 01:57:21 +0100

    emad 2.3 working updated with comments

--- implementations/2.3SortAndSearchBenchmark.c
+++ implementations/2.3SortAndSearchBenchmark.c
@@ -9,14 +9,10 @@
 #include "quicksortInt.h"
 #include "binarySearchAlgorithm.h"
 
-//generate a random struct with 30000 elements
-//the max length of the string is 10
-//the max value of the integer is 500
-
-int genRanBigStruct(int valuesCount, struct data workingStruct[]){
+//generate a random struct with given elements
+void genRanBigStruct(int valuesCount, struct data workingStruct[]){
     for(int i=0; i < valuesCount; i++) {
         workingStruct[i].number = randNumReturn(3000,0);
-        //workingStruct[i].number = i;
         char randomletter[6]={};
         for(int c=0; c<5; c++){
 
@@ -28,6 +24,7 @@ int genRanBigStruct(int valuesCount, struct data workingStruct[]){
 
 }
 
+//compare function for the qsort to use
 int cmpStruct(const void *a, const void *b) {
     struct data *ia = (struct data *)a;
     struct data *ib = (struct data *)b;
@@ -35,20 +32,27 @@ int cmpStruct(const void *a, const void *b) {
 }
 
 int main() {
+    //set seed for rand funcion
     srand(time(NULL));
     int matchescounter = 0;
     int printingSize = 100;
     int tempArraySize = 500;
     int structDataCount = 30000;
+    //allocate memory for temporary array
     int *tempArr = malloc((sizeof(int) * tempArraySize));
     if(tempArr == NULL){
         printf("error Malloc 1");
         exit(1);
     }
     printf("=============================================LIB FUNCTIONS BEG =============================================\n");
+
+    //generate given number of random positive numbers in the array
     genRandPosNum(tempArraySize,tempArr,5000);
+    //create structure
     struct data workingStruct[structDataCount];
+    //fill structure with random values
     genRanBigStruct(structDataCount, workingStruct);
+
     printf("\n=========UNSORTED========\n");
     for (int i = 0; i < printingSize; i++) {
         printf("[i:%-3d] %-5d %s | ",i, workingStruct[i].number, workingStruct[i].string);
@@ -57,10 +61,14 @@ int main() {
 
     }
     printf("\n=========END UNSORTED========\n");
+
+    //measure time taken
     clock_t start, end;
     double cpu_time_used;
     start = clock();
+    //use lib qsort to sort the first struct
     qsort(workingStruct,structDataCount,sizeof(struct data), cmpStruct);
+    //for every element in the array search the struct
     for(int i=0; i < tempArraySize; i++){
         //create a temporary struct to store the found element and fill it with the return of the binarysearch lib function
         struct data* structIndex = bsearch(&tempArr[i], workingStruct, structDataCount, sizeof(struct data), cmpStruct);
@@ -70,7 +78,9 @@ int main() {
         }
     }
     end = clock();
+    //calculate time taken
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
     printf("\n=========SORTED========\n");
     for (int i = 0; i < printingSize; i++) {
         printf("[i:%-3d] %-5d %s | ",i, workingStruct[i].number, workingStruct[i].string);
@@ -82,32 +92,40 @@ int main() {
     printf("Time taken by library functions: %f\n", cpu_time_used);
     printf("Found %d matches.\n", matchescounter);
     printf("=============================================LIB FUNCTIONS END =============================================\n");
+
     printf("=============================================OWN FUNCTIONS BEG =============================================\n");
-    struct data workingStruct2[structDataCount];
-    genRanBigStruct(structDataCount, workingStruct2);
+
+    //regenerate structure
+    genRanBigStruct(structDataCount, workingStruct);
+
     printf("\n=========UNSORTED========\n");
     for (int i = 0; i < printingSize; i++) {
-        printf("[i:%-3d] %-5d %s | ",i, workingStruct2[i].number, workingStruct2[i].string);
+        printf("[i:%-3d] %-5d %s | ",i, workingStruct[i].number, workingStruct[i].string);
         if((i+1)%5 == 0)
             printf("\n");
 
     }
     printf("\n=========END UNSORTED========\n");
+
+    //measure time taken
     start = clock();
-    quickSortInt(workingStruct2, structDataCount);
+    //sort struct with our own quicksort
+    quickSortInt(workingStruct, structDataCount);
     matchescounter = 0;
+    //for every element in the array search the struct
     for(int i=0; i < tempArraySize; i++){
-        int returnIndex = binarySearchInteger(workingStruct2, tempArr[i], 0, structDataCount);
+        int returnIndex = binarySearchInteger(workingStruct, tempArr[i], 0, structDataCount);
         if(returnIndex >= 0){
             matchescounter++;
         }
     }
     end = clock();
+    //calculate time taken
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 
     printf("\n=========SORTED========\n");
     for (int i = 0; i < printingSize; i++) {
-        printf("[i:%-3d] %-5d %s | ",i, workingStruct2[i].number, workingStruct2[i].string);
+        printf("[i:%-3d] %-5d %s | ",i, workingStruct[i].number, workingStruct[i].string);
         if((i+1)%5 == 0)
             printf("\n");
 
@@ -116,6 +134,9 @@ int main() {
     printf("Time taken by our functions: %f\n", cpu_time_used);
     printf("Found %d matches.\n", matchescounter);
     printf("=============================================OWN FUNCTIONS END =============================================\n");
+
+    //free allocated memory
+    free(tempArr);
     return 0;
 }
 
